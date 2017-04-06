82eee0e92e2521a4928c5977fac710eb7641bdaa
msmiri <msmiri@goalsystems.com>
2017-04-06 18:01:26 +0200

    second

---
+++ DemoJenkins/Models/ContextClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoJenkins.Models
+{
+    public class ContextClass: DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ DemoJenkins/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoJenkins.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string ItemName { get; set; }
+    }
+}
